aadde02e48c75bc9c58295a43ae3fbeb7866acae
isarebollo <isarebollo@gmail.com>
2022-05-17 08:18:22 +0000

    Card Poker Random

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const btn = document.querySelector("button");
+btn.addEventListener("click", function hola() {
+  const clases = ["♦", "♥", "♠", "♣"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let index = Math.floor(Math.random() * clases.length);
+  let newClass = clases[index];
+
+  var carta = document.getElementById("carta");
+  if (newClass === "♦" || newClass === "♥") {
+    carta.className = "rojo";
+  } else {
+    carta.className = "negro";
+  }
+  document.getElementById("uno").innerHTML = newClass;
+  document.getElementById("tres").innerHTML = newClass;
+  const number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let randomNumber = Math.floor(Math.random() * number.length);
+  let newNumber = number[randomNumber];
+  document.getElementById("dos").innerHTML = newNumber;
+});
